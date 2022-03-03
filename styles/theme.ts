1de526200d56af2af731d105123ebf44e60d3946
Matheus Bruggemann Vieira <matheusbvieira@hotmail.com>
2022-03-03 15:27:53 -0300

    Cria tema

---
+++ styles/theme.ts
@@ -0,0 +1,11 @@
+import { DefaultTheme } from "styled-components";
+
+export const light: DefaultTheme = {
+
+    pageBackground: "#FFFFFF",
+    pageForeground: "#000000",
+    primaryBackground: "#66BB6A",
+    primaryForeground: "#FFFFFF",
+    attentionForeground: "#338A3E",
+    
+}
