9a816b485d8dd873c6ee64f65abb48a0022a9585
Matheus Bruggemann Vieira <matheusbvieira@hotmail.com>
2022-04-05 13:50:17 -0300

    Remove styled component svg

--- styles/globalStyles.ts
+++ styles/globalStyles.ts
@@ -33,4 +33,14 @@ export default createGlobalStyle`
   .error {
     border-bottom: 2px solid red;
   }
+
+  .imagemLocal {
+    width: 628px;
+    height: 834px;
+
+    @media only screen and (max-width: 500px) {
+      width: 338px;
+      height: 271px;
+    }
+  }
 `;
